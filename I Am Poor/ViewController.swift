c055758f820f6269895565de82a7cbf6286f0530
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2020-11-20 21:33:46 +0530

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Dhanraj Naik on 20/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
